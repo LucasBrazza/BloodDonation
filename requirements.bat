a7fb17bde3ecdb30bdb81b08e25e9f310c3f9a21
Lucas Brazzarola <lucas.brazza.lb@gmail.com>
2024-02-15 09:14:12 -0300

    Update requirements.bat

--- requirements.bat
+++ requirements.bat
@@ -2,7 +2,7 @@ python -m venv .vemv
 .venv\Scripts\activate
 pip install django
 pip install djangorestframework
-pip install django-bootstrap5
+pip install django-bootstrap-v5
 pip install django-crispy-forms
 python manage.py makemigrations
 python manage.py migrate
